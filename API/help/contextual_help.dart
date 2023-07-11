1804e4d4b89ea4d62df7a969f2f80d7c4fb89a7d
Bigmints <66945811+bigmints@users.noreply.github.com>
2023-07-11 13:24:38 +0400

    Create contextual_help.dart

---
+++ API/help/contextual_help.dart
@@ -0,0 +1,35 @@
+{
+  "topics":[
+    {
+      "topic": "link-editing",
+     
+      "title": "title text",
+      "description": "description",
+      "content":[
+        {
+        "title":"title",
+        "description":"description"
+        },
+        {
+        "title":"title",
+        "description":"description"
+        },{
+        "title":"title",
+        "description":"description"
+        }
+      ],
+      "media":{
+        "image":{
+          "name": "image name",
+          "URL": "url"
+        },
+        "video":{
+          "name": "image name",
+          "URL": "url"
+        }
+      }
+    
+    }
+    
+  ]
+}
